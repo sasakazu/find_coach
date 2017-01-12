9210c55914e52955317c9b3c7849cdadfd6b4b6a
sasakura <dropout.college2014@gmail.com>
2017-01-12 04:06:45 +0000

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,6 +6,31 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
+  
+  
+  # 記憶トークンcookieに対応するユーザーを返す
+  
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+  
+  
+  # ユーザーを永続的セッションに記憶する
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  
 # 現在ログイン中のユーザーを返す (いる場合)
 
   def current_user
@@ -18,9 +43,16 @@ module SessionsHelper
     !current_user.nil?
   end
   
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   # 現在のユーザーをログアウトする
-  
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111045545) do
+ActiveRecord::Schema.define(version: 20170112034914) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,8 +18,8 @@ ActiveRecord::Schema.define(version: 20170111045545) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
